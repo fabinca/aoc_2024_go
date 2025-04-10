67fc3a7dbb7cb7ce2a80ab941a444044a444c908
Carlotta Fabian <c.fabian@turbit.de>
2025-04-10 20:24:05 +0200

    utils0update

--- aoc_utils/types.go
+++ aoc_utils/types.go
@@ -6,6 +6,20 @@ type Coordinate struct {
 	Symbol rune
 }
 
+func (a *Coordinate) Add(b Coordinate) {
+	a.Row += b.Row
+	a.Col += b.Col
+}
+
+func (a *Coordinate) Substract(b Coordinate) {
+	a.Row -= b.Row
+	a.Col -= b.Col
+}
+
+func (a Coordinate) Equals(b Coordinate) bool {
+	return a.Row == b.Row && a.Col == b.Col
+}
+
 type Pair struct {
 	A string
 	B string
